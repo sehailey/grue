852ebed55dff12dfc75043c9de4b18a402792314
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-09-17 15:38:31 -0400

    added redux

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from "redux"
+import { createLogger } from "redux-logger"
+import thunkMiddleware from "redux-thunk"
+import { composeWithDevTools } from "redux-devtools-extension"
+
+import items from "./reducers"
+
+const reducer = combineReducers({ items })
+//const reducer = items
+const middleware = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+)
+const store = createStore(reducer, middleware)
+
+export default store
+export * from "./reducers"
