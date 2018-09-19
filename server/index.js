32523307f786d16bcd79721fab82c8b7a346f7d6
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-09-19 01:00:17 -0400

    heroku

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require("express")
-
+const path = require("path")
 const app = express()
 const port = process.env.PORT || 1337
 
 app.use("/api", require("./api"))
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+}
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"))
+})
 
 app.listen(port, () => console.log(`Doin' haxor stuff on port ${port}`))
