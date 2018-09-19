925b0c148ff33ca4d00845f03094a75f6663a709
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-09-19 14:22:04 -0400

    added newlines to room descriptions

--- src/components/rooms.js
+++ src/components/rooms.js
@@ -1,21 +1,21 @@
 const cave = {
   name: "cave",
-  description: "It is pitch black. you are likely to be eaten by a grue."
+  description: "It is pitch black. you are likely to be eaten by a grue.\n"
 }
 
 const outside = {
   name: "outside",
-  description: "You are outside."
+  description: "You are outside.\n"
 }
 
 const path = {
   name: "path",
-  description: "You are on a long, winding path"
+  description: "You are on a long, winding path.\n"
 }
 
 const house = {
   name: "house",
-  description: "You're at a house. There's a mailbox."
+  description: "You're at a house. There's a mailbox.\n"
 }
 
 export default [cave, outside, path, house]
