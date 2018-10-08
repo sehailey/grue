dae5ebe6f06fc33f88fdc8d75bf5c749b8ebdfc2
notnull <notnull@host.home>
2018-10-08 14:42:38 -0400

    added more objects

--- server/api/rooms.js
+++ server/api/rooms.js
@@ -1,12 +1,12 @@
-const router = require("express").Router()
-const { Item, Room } = require("../db/models")
+const router = require('express').Router()
+const { Item, Room } = require('../db/models')
 module.exports = router
 
-router.get("/", async (req, res, next) => {
-  try {
-    const rooms = await Room.findAll({ include: Item })
-    res.json(rooms)
-  } catch (err) {
-    next(err)
-  }
+router.get('/', async (req, res, next) => {
+    try {
+        const rooms = await Room.findAll({ include: Item })
+        res.json(rooms)
+    } catch (err) {
+        next(err)
+    }
 })
